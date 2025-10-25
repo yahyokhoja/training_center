a827c659ef203cdaceedd3dcf5391d0ee000f5e2
yahyokhoja <yahuokhoja@gmail.com>
2025-10-25 23:31:48 +0500

    Add Render domain to ALLOWED_HOSTS

--- training_center/settings.py
+++ training_center/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-*l*klf$gfixv-as*&=21gv1+-p4-pseac(okqtk1!3+dapl0)%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'training-center-6j6m.onrender.com']
+
 
 
 # Application definition
