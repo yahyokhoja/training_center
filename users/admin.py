193fbf744630c01d405f0f8cf40be7d5921714b2
yahyokhoja <yahuokhoja@gmail.com>
2025-11-04 23:04:30 +0500

    Add CustomUser model with phone field and configure admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Поля, отображаемые в списке пользователей
+    list_display = ('username', 'email', 'phone', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')  # 'role' убран, так как его нет
+
+    # Поля для редактирования существующего пользователя
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone',)}),
+    )
+
+    # Поля при создании нового пользователя
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone',)}),
+    )
+
+    search_fields = ('username', 'email', 'phone')
+    ordering = ('username',)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=20, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.username or self.phone
